de19f7361f47f638b390f729207989b137344e2a
JustinaGalarce <justina.galarec@ing.austral.edu.ar>
2022-11-04 12:58:14 -0300

    soy yo, la justa

---
+++ src/screens/Camillero/TrasladoTermiando.js
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+import * as React from "react";
+
+export const TrasladoTerminado = () => {
+    return (
+        <div style={{display:"flex", flexDirection:"column",backgroundImage:"url:https://blog.laboralkutxa.com/src/uploads/2018/05/Reconocimiento-facial.jpg"}}>
+            <h1> ¿Termino su traslado? </h1>
+            <Button variant="contained" color="success">
+                SI
+            </Button>
+        </div>
+    );
+}
